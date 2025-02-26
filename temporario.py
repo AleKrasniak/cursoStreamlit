fc8f465b13fb649ff3b1f1beafc10b0bcb37f9e2
AleKrasniak <142438661+AleKrasniak@users.noreply.github.com>
2025-02-26 20:01:26 -0300

    Update temporario.py
    Select box integrado

--- temporario.py
+++ temporario.py
@@ -4,3 +4,6 @@ st.toggle("Toggle")
 st.button("Botão Salvar")
 st.text_area("Adicione texto")
 st.text_input("")
+st.selectbox(
+  'Do que é feito o Teteu?',
+  ('Acuçar 100%','50% Acuçar','50% Açucar e 50% Mel'))
