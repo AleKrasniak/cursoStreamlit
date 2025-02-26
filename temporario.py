f28a47f26c1d9b2bce53bdb0f32ffd9acaed18bc
AleKrasniak <142438661+AleKrasniak@users.noreply.github.com>
2025-02-26 20:07:21 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -2,8 +2,8 @@ import streamlit as st
 st.header("Cabeçalho")
 st.multiselect(
   'Números da Mega Sena',
-  ['31','62','98','100','20','12'],
-  ['81','52'])
+  ['31', '62', '98', '100', '20', '12'],
+  ['81', '52'])
 st.toggle("Toggle")
 st.button("Botão Salvar")
 st.text_area("Adicione texto")
