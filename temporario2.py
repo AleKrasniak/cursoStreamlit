efcfa1fae83bdeebb64eb8c3232719d448b99f66
AleKrasniak <142438661+AleKrasniak@users.noreply.github.com>
2025-02-28 20:37:17 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -5,12 +5,12 @@ st.header("Informe Seus Dados")
 col1, col2 = st.columns(2)
 
 with col1:
-with st.expander
-  uploaded_files = st.file_uploader(
-    "Escolha uma foto", accept_multiple_files=True
-  )
-  if uploaded_files is not None: 
-    try:
-      uploaded_files = Image.open(uploaded_files)
-    except: 
-      st.error("Imagem não enviada")
+  with st.expander
+    uploaded_files = st.file_uploader(
+      "Escolha uma foto", accept_multiple_files=True
+    )
+    if uploaded_files is not None: 
+      try:
+        uploaded_files = Image.open(uploaded_files)
+      except: 
+        st.error("Imagem não enviada")
