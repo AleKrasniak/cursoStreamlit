10e3ecd643a28f94afcbf618fa7a5cd79e517ffb
AleKrasniak <142438661+AleKrasniak@users.noreply.github.com>
2025-02-28 20:14:33 -0300

    Update temporario2.py
    coluna + imagem add

--- temporario2.py
+++ temporario2.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.button("Reset")
+st.header("Informe Seus Dados")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  uploadedfiles = st.file_uploader(
+    "Escolha uma foto" accept_multiple_files=true
+  )
+
+  for uploaded_file in uploaded_files:
+  bytes_data = uploaded_file.read()
+  st.write("filename:", uploaded_file.name
+  st.write (bytes_data)
