b758db9fd0006b6963d29fc19c6b085c63156067
AleKrasniak <142438661+AleKrasniak@users.noreply.github.com>
2025-02-28 21:12:45 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -5,13 +5,47 @@ st.header("Informe Seus Dados")
 col1, col2 = st.columns(2)
 
 with col1:
-    uploaded_files = st.file_uploader(
-      "Escolha uma foto", accept_multiple_files=True
-    )
-    if uploaded_files is not None: 
-      try:
-        uploaded_files = Image.open(uploaded_files)
-      except: 
-        st.error("Imagem não enviada")
+with upload_tab:
+    file = st.file_uploader("Upload Art", key="file_uploader")
+    if file is not None:
+        try:
+            img = Image.open(file)
+        except:
+            st.error("The file you uploaded does not seem to be a valid image. Try uploading a png or jpg file.")
+    if st.session_state.get("image_url") not in ["", None]:
+        st.warning("To use the file uploader, remove the image URL first.")
+
+with url_tab:
+    url_text = st.empty()
+    
+    # FIXME: the button is a bit buggy, but it's worth fixing this later
+
+    # url_reset = st.button("Clear URL", key="url_reset")
+    # if url_reset and "image_url" in st.session_state:
+    #     st.session_state["image_url"] = ""
+    #     st.write(st.session_state["image_url"])
+
+    url = url_text.text_input("Image URL", key="image_url")
+    
+    if url!="":
+        try:
+            response = requests.get(url)
+            img = Image.open(BytesIO(response.content))
+        except:
+            st.error("The URL does not seem to be valid.")
+
+# convert RGBA to RGB if necessary
+n_dims = np.array(img).shape[-1]
+if n_dims == 4:
+    background = Image.new("RGB", img.size, (255, 255, 255))
+    background.paste(img, mask=img.split()[3]) # 3 is the alpha channel
+    img = background
+
+# apply image enhancements
+for cat in enhancement_categories:
+    img = getattr(ImageEnhance, cat)(img)
+    img = img.enhance(enhancement_factor_dict[cat])
+
+# show the image
 with st.expander("🖼  Artwork", expanded=True):
     st.image(img, use_column_width=True)
