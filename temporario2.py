e8bd7a43548a068ef01f7b515be57ad04b001b90
AleKrasniak <142438661+AleKrasniak@users.noreply.github.com>
2025-02-28 21:20:05 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -1,120 +1,21 @@
-import os
-import sys
-import pandas as pd
-import numpy as np 
-import requests
-
-from io import BytesIO
-from glob import glob
-from PIL import Image, ImageEnhance
-
 import streamlit as st
+import pandas as pd
+from io import StringIO
 
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
 
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
 
-gallery_files = glob(os.path.join(".", "images", "*"))
-gallery_dict = {image_path.split("/")[-1].split(".")[-2].replace("-", " "): image_path
-    for image_path in gallery_files}
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
 
-st.image("logo.jpg")
-st.sidebar.title("Sophisticated Palette 🎨")
-st.sidebar.caption("Tell your data story with style.")
-st.sidebar.markdown("Made by [Siavash Yasini](https://www.linkedin.com/in/siavash-yasini/)")
-st.sidebar.caption("Look behind the scenes of Sophisticated Palette [here](https://blog.streamlit.io/create-a-color-palette-from-any-image/).")
-
-
-with st.sidebar.expander("See My Other Streamlit Apps"):
-    st.caption("Snowflake Cheat Sheet: [App](https://snow-flake-cheat-sheet.streamlit.app/) 🎈,  [Blog Post](https://medium.com/snowflake/the-ungifted-amateurs-guide-to-snowflake-449284e4bd72) 📝")
-    st.caption("Wordler: [App](https://wordler.streamlit.app/) 🎈,  [Blog Post](https://blog.streamlit.io/the-ultimate-wordle-cheat-sheet/) 📝")
-    st.caption("Koffee of the World: [App](https://koffee.streamlit.app/) 🎈")
-
-st.sidebar.markdown("---")
-
-toggle = st.sidebar.checkbox("Toggle Update", value=True, help="Continuously update the pallete with every change in the app.")
-click = st.sidebar.button("Find Palette", disabled=bool(toggle))
-
-st.sidebar.markdown("---")
-st.sidebar.header("Settings")
-palette_size = int(st.sidebar.number_input("palette size", min_value=1, max_value=20, value=5, step=1, help="Number of colors to infer from the image."))
-sample_size = int(st.sidebar.number_input("sample size", min_value=5, max_value=3000, value=500, step=500, help="Number of sample pixels to pick from the image."))
-
-# Image Enhancement
-enhancement_categories = enhancement_range.keys()
-enh_expander = st.sidebar.expander("Image Enhancements", expanded=False)
-with enh_expander:
-    
-    if st.button("reset"):
-        for cat in enhancement_categories:
-            if f"{cat}_enhancement" in st.session_state:
-                st.session_state[f"{cat}_enhancement"] = 1.0
-enhancement_factor_dict = {
-    cat: enh_expander.slider(f"{cat} Enhancement", 
-                            value=1., 
-                            min_value=enhancement_range[cat][0], 
-                            max_value=enhancement_range[cat][1], 
-                            step=enhancement_range[cat][2],
-                            key=f"{cat}_enhancement")
-    for cat in enhancement_categories
-}
-enh_expander.info("**Try the following**\n\nColor Enhancements = 2.6\n\nContrast Enhancements = 1.1\n\nBrightness Enhancements = 1.1")
-
-# Clustering Model 
-model_name = st.sidebar.selectbox("machine learning model", model_dict.keys(), help="Machine Learning model to use for clustering pixels and colors together.")
-sklearn_info = st.sidebar.empty()
-
-sort_options = sorted(list(sort_func_dict.keys()) + [key + "_r" for key in sort_func_dict.keys() if key!="random"])
-sort_func = st.sidebar.selectbox("palette sort function", options=sort_options, index=5)
-
-# Random Number Seed
-seed = int(st.sidebar.number_input("random seed", value=42, help="Seed used for all random samplings."))
-np.random.seed(seed)
-st.sidebar.markdown("---")
-
-
-# =======
-#   App
-# =======
-
-# provide options to either select an image form the gallery, upload one, or fetch from URL
-gallery_tab, upload_tab, url_tab = st.tabs(["Gallery", "Upload", "Image URL"])
-with gallery_tab:
-    options = list(gallery_dict.keys())
-    file_name = st.selectbox("Select Art", 
-                            options=options, index=options.index("Mona Lisa (Leonardo da Vinci)"))
-    file = gallery_dict[file_name]
-
-    if st.session_state.get("file_uploader") is not None:
-        st.warning("To use the Gallery, remove the uploaded image first.")
-    if st.session_state.get("image_url") not in ["", None]:
-        st.warning("To use the Gallery, remove the image URL first.")
-
-    img = Image.open(file)
-
-with upload_tab:
-    file = st.file_uploader("Upload Art", key="file_uploader")
-    if file is not None:
-        try:
-            img = Image.open(file)
-        except:
-            st.error("The file you uploaded does not seem to be a valid image. Try uploading a png or jpg file.")
-    if st.session_state.get("image_url") not in ["", None]:
-        st.warning("To use the file uploader, remove the image URL first.")
-
-with url_tab:
-    url_text = st.empty()
-    
-    # FIXME: the button is a bit buggy, but it's worth fixing this later
-
-    # url_reset = st.button("Clear URL", key="url_reset")
-    # if url_reset and "image_url" in st.session_state:
-    #     st.session_state["image_url"] = ""
-    #     st.write(st.session_state["image_url"])
-
-    url = url_text.text_input("Image URL", key="image_url")
-    
-    if url!="":
-        try:
-            response = requests.get(url)
-            img = Image.open(BytesIO(response.content))
-        except:
-            st.error("The URL does not seem to be valid.")
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
