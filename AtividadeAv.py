b6478dc3bc149695a8dcbb1a7a6ae4d589ef0424
AleKrasniak <142438661+AleKrasniak@users.noreply.github.com>
2025-03-28 20:35:53 -0300

    Update AtividadeAv.py

--- AtividadeAv.py
+++ AtividadeAv.py
@@ -1,11 +1,16 @@
 import streamlit as st 
 
-print("peido")
+
 
 st.title("Programa Aumento Salarial")
 
 
 # container = st.container (border=True) 
-with st.container(border = True):
-  Nome = st.text_input("Informe seu nome","Nome")
+def ExibirDados():
+  with st.container(border = True):
+    Nome = st.text_input("Informe seu nome",)
+  with st.container(border = True):
+    DataNasc = st.date.input("Informe sua data de nascimento")
+
+
 
