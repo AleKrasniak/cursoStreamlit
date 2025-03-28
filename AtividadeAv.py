85ffacc1db0a3923a0ef0263e874f3bf69a86514
AleKrasniak <142438661+AleKrasniak@users.noreply.github.com>
2025-03-28 20:29:29 -0300

    Update AtividadeAv.py

--- AtividadeAv.py
+++ AtividadeAv.py
@@ -6,6 +6,6 @@ st.title("Programa Aumento Salarial")
 
 
 # container = st.container (border=True) 
-with st.container():
+with st.container(border = True):
   Nome = st.text_input("Informe seu nome","Nome")
 
