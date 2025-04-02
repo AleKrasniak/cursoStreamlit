89bb3f66c703c917fc3d273e1e867ecc98c9d6e7
AleKrasniak <142438661+AleKrasniak@users.noreply.github.com>
2025-04-02 20:13:21 -0300

    Update AtividadeAv.py

--- AtividadeAv.py
+++ AtividadeAv.py
@@ -9,17 +9,25 @@ st.title("Programa Aumento Salarial")
 def ExibirDados():
   with st.container(border = True):
     Nome = st.text_input("Informe seu nome",)
+    
   with st.container(border = True):
     DataNasc = st.date_input("Informe sua data de nascimento")
+    
   with st.container(border = True):
     EstadoCivil = st.selectbox(
       "Informe seu estado cívil", 
       ("Solteiro", "Casado", "Divorciado", "Viúvo"),
     )
+    
   with st.container(border = True):
     Sexo = st.selectbox(
       "Informe seu gênero", 
       ("Masculino", "Feminino",),
+    )
+    
+  with st.container(border = True):
+    Salario = st.number_input(
+      "Informe seu salário", 
     )
     
 #SELECT BOX
