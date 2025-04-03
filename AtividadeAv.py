be5c50bd37b599d7fd932bea01a3dff354e88ff4
AleKrasniak <142438661+AleKrasniak@users.noreply.github.com>
2025-04-02 21:27:54 -0300

    Update AtividadeAv.py

--- AtividadeAv.py
+++ AtividadeAv.py
@@ -32,9 +32,9 @@ def main():
   if st.button("teste"): 
    if Salario < 2500:
     st.write("Deve receber Aumento")
-   while aumento < 500:
-    aumento = aumento + 100
-    st.write("Seu aumento foi de R$: ",aumento,)
+     while (aumento < 500):
+      aumento = aumento + 100
+      st.write("Seu aumento foi de R$: ",aumento,)
 
    else:
     st.write("peido")
