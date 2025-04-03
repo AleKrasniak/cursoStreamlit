ee84eaf32929b41b7354d19915780c17402e201b
AleKrasniak <142438661+AleKrasniak@users.noreply.github.com>
2025-04-02 22:01:52 -0300

    Update AtividadeAv.py

--- AtividadeAv.py
+++ AtividadeAv.py
@@ -33,7 +33,7 @@ def main():
      st.write("Deve receber Aumento")
  
      aumento = 0.0
-     while (aumento <= 500):
+     while (aumento < 500):
       aumento = aumento + 100
       st.write("Seu aumento foi de R$: ",aumento,)
  
